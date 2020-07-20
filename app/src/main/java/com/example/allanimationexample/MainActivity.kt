bdf941cc702f42d1eeb0f600ba43c77cd081d67c
Ehsan Fallahi <ehsanfallahi@gmail.com>
2020-07-20 18:16:45 +0430

    add rotation animation

--- app/src/main/java/com/example/allanimationexample/MainActivity.kt
+++ app/src/main/java/com/example/allanimationexample/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.allanimationexample
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.example.allanimationexample.databinding.ActivityMainBinding
 
@@ -12,6 +16,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
 
+        binding.apply {
+            btnRotate.setOnClickListener {
+                rotaion()
+            }
+        }
+
+    }
+
+    private fun rotaion() {
+        val animator=ObjectAnimator.ofFloat(binding.imgStar,View.ROTATION,
+            -360f,0f)
+        animator.apply {
+            duration=2000
+            addListener(object:AnimatorListenerAdapter(){
+                override fun onAnimationEnd(animation: Animator?) {
+                    binding.btnRotate.isEnabled=true
+                }
+                override fun onAnimationStart(animation: Animator?) {
+                    binding.btnRotate.isEnabled=false
+                }
+            })
+            start()
+        }
 
     }
 }
