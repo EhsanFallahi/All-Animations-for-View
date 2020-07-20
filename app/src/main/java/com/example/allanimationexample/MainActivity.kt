6e99cc29be1996a7b9d1ae330ef5bff85ac68dc1
Ehsan Fallahi <ehsanfallahi@gmail.com>
2020-07-20 18:29:49 +0430

    add translater animation and create extension function for disable view during animation

--- app/src/main/java/com/example/allanimationexample/MainActivity.kt
+++ app/src/main/java/com/example/allanimationexample/MainActivity.kt
@@ -20,8 +20,35 @@ class MainActivity : AppCompatActivity() {
             btnRotate.setOnClickListener {
                 rotaion()
             }
+            btnTranslate.setOnClickListener {
+                translater()
+            }
         }
 
+    }
+
+    private fun ObjectAnimator.disableViewDuringAnimation(view: View){
+        addListener(object :AnimatorListenerAdapter(){
+            override fun onAnimationEnd(animation: Animator?) {
+                view.isEnabled=true
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+                view.isEnabled=false
+            }
+
+        })
+    }
+
+    private fun translater() {
+        val animator=ObjectAnimator.ofFloat(binding.imgStar,View.TRANSLATION_X,300f)
+        animator.apply {
+            duration=1000
+            repeatCount=1
+            repeatMode=ObjectAnimator.REVERSE
+            disableViewDuringAnimation(binding.btnTranslate)
+            start()
+        }
     }
 
     private fun rotaion() {
@@ -29,14 +56,7 @@ class MainActivity : AppCompatActivity() {
             -360f,0f)
         animator.apply {
             duration=2000
-            addListener(object:AnimatorListenerAdapter(){
-                override fun onAnimationEnd(animation: Animator?) {
-                    binding.btnRotate.isEnabled=true
-                }
-                override fun onAnimationStart(animation: Animator?) {
-                    binding.btnRotate.isEnabled=false
-                }
-            })
+            disableViewDuringAnimation(binding.btnRotate)
             start()
         }
 
